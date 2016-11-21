7355e120f134b0a321744f02cb202fec1cabd6c0
Felix Liang <lys74123@163.com>
2016-11-21 16:36:22 +0800

    安卓各Sensor使用演示

--- app/src/main/java/com/felix/sensordemo/view/AccelerometerActivity.java
+++ app/src/main/java/com/felix/sensordemo/view/AccelerometerActivity.java
@@ -1,5 +1,7 @@
 package com.felix.sensordemo.view;
 
+import android.os.Bundle;
+
 import com.felix.sensordemo.R;
 import com.felix.sensordemo.app.BaseActivity;
 
@@ -11,5 +13,20 @@ public class AccelerometerActivity extends BaseActivity {
     @Override
     protected int getLayoutResID() {
         return R.layout.activity_accelerometer;
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected void initData(Bundle savedInstanceState) {
+
+    }
+
+    @Override
+    protected void initListener() {
+
     }
 }
