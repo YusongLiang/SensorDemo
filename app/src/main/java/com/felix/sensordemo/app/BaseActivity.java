5a182c1f4c8c998fb2cd2fb023cf9c2acf89b346
Felix Liang <lys74123@163.com>
2016-11-24 17:30:08 +0800

    安卓各Sensor使用演示

--- app/src/main/java/com/felix/sensordemo/app/BaseActivity.java
+++ app/src/main/java/com/felix/sensordemo/app/BaseActivity.java
@@ -1,8 +1,11 @@
 package com.felix.sensordemo.app;
 
+import android.app.AlertDialog;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+
+import com.felix.sensordemo.R;
 
 /**
  * @author Felix
